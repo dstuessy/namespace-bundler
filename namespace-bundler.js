ab0bae6cc3915ed23d18da197428d7d4dc7b085e
Daniel <danielstuessy@gmail.com>
2016-09-17 22:14:38 +0100

    Parse files for variable names under namespace

--- namespace-bundler.js
+++ namespace-bundler.js
@@ -1,9 +1,47 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (function () {
     'use strict';
 
     const Bundler = {};
+
+    function cloneObj(obj) {
+        let proto = Object.getPrototypeOf(obj);
+        let clone = Object.create(proto);
+
+        return Object.keys(obj).reduce((clone, key) => {
+            let val = obj[key];
+            clone[key] = val;
+            return clone;
+        }, clone);
+    }
+
+    function objectSet(obj, key, val) {
+        let clone = cloneObj(obj);
+        clone[key] = val;
+        return clone;
+    }
+
+    function isUnique(filePath, i, list) {
+        return list.indexOf(filePath) === i;
+    }
+
+    function getVarName(filePath) {
+        let fileContent = fs.readFileSync(filePath).toString();
+        let fileName = path.basename(filePath, '.js');
+        let regexEscapedFileName = fileName.replace(/\./g, '\\.');
+        let varRegEx = RegExp(`(${regexEscapedFileName})\ ?=`, 'i');
+
+        return fileContent.match(varRegEx)[1] || "";
+    }
+
+    function getDependencies(filePath, fileNames) {
+        let fileContent = fs.readFileSync(filePath).toString();
+        let dependencyRegex = RegExp(`(${file})`, 'i');
+
+        return fileContent;
+    }
 
     function getFilePaths(dirPath) {
         let entryNames = fs.readdirSync(dirPath);
@@ -17,13 +55,25 @@ module.exports = (function () {
 
         return filePaths
             .concat(leftoverFilePaths)
-            .filter((filePath, i, self) => self.indexOf(filePath) === i);
+            .filter(isUnique);
     }
 
     Bundler.bundle = function bundle(dirPath, callback) {
         let filePaths = getFilePaths(dirPath, callback);
+        let fileModules = filePaths.map(filePath => {
+            return {
+                filePath: filePath,
+                varName: "",
+                dependencies: []
+            }
+        }).map(fileModule => objectSet(
+            fileModule, "varName", getVarName(fileModule.filePath)
+        ));
+        // .map(fileModule => objectSet(
+        //     fileModule, "dependencies", getDependencies(fileModule.filePath)
+        // ));
 
-        console.log(filePaths);
+        console.log(getDependencies(fileModules[0].filePath));
     };
 
     return Bundler;
